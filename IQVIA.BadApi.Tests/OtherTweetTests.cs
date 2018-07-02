014729ad94817cdf54fd93872dd098fbe008aad6
Paul Torvik <paulnospam@gmail.com>
2018-07-02 13:12:59 -0700

    Added test case expecting no tweets to be returned because it's a boundary condition

---
+++ IQVIA.BadApi.Tests/OtherTweetTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IQVIA.BadApi.Client;
+
+namespace IQVIA.BadApi.Tests
+{
+    [TestClass]
+    public class OtherTweetTests
+    {
+        /// <summary>
+        /// Test case expecting no returned tweets
+        /// </summary>
+        [TestMethod]
+        public void NoTweets()
+        {
+            var badApiClient = new BadApiClient();
+            var tweets = badApiClient.GetAllTweetsAsync(
+                DateTime.MinValue.ToUniversalTime(),
+                DateTime.MinValue.ToUniversalTime()).Result;
+            Assert.IsNotNull(tweets, "Tweets list is null");
+            Assert.IsFalse(tweets.Any(), "Not expecting any tweets to be returned");
+        }
+    }
+}
